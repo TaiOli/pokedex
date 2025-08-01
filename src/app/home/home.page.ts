9ff50a7d7cdd959200742544ab2ab69e360e7ddb
Tais <taiscoliveiratec@gmail.com>
2025-07-31 21:09:14 -0300

    tela principal com o nome e imagem do pokemon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { PokeapiService } from '../services/pokeapi.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [CommonModule, IonicModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  pokemon: any = null;
+  pokemonName: string = 'pikachu';
+
+  constructor(private pokeService: PokeapiService) {}
+
+  ngOnInit() {
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    this.pokeService.getPokemon(this.pokemonName).subscribe({
+      next: (data) => this.pokemon = data,
+      error: (err) => console.error(err),
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { IonicModule } from '@ionic/angular';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(IonicModule.forRoot()),
   ],
 });
